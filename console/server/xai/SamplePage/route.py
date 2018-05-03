f9ead66fcdb143bf51ecaee76a485bd7c0008a02
jderlin <jder@fungogo.xyz>
2018-05-03 10:01:41 +0800

    update

--- console/server/xai/SamplePage/route.py
+++ console/server/xai/SamplePage/route.py
@@ -21,28 +21,6 @@ def addSubscriber():
 		'foo': 'bar'
 	})
 
-@app.route("/"+app.config['API_VERSION']+"/test", methods=['GET'])
-def test():
-	return jsonify({
-		'test': 'test',
-		'test': 'false'
-	})
-
-@app.route("/"+app.config['API_VERSION']+"/bool", methods=['GET'])
-def bool():
-	stores = [{
-    'name': 'Elton\'s first store',
-    'items': [{'name':'my item 1', 'price': 30 }],
-    'dda': True,
-    },
-    {
-    'name': 'Elton\'s second store',
-    'items': [{'name':'my item 2', 'price': 15 }],
-    'dda': False,
-    },
-]
-	return jsonify(stores)			
-
 @app.route("/"+app.config['API_VERSION']+"/reverse_echo", methods=['GET'])
 def reverse_echo():
 	s = request.args.get('s')
@@ -60,31 +38,70 @@ def reverse_echo():
 
 @app.route("/"+app.config['API_VERSION']+"/filters", methods=['GET'])
 def filters():
-	return jsonify([
+	filters = [
 		{
 		'id': '9beb08c9-5069-4cfb-8ff9-ce8f5fbf4c07',
 		'name': '餐廳',
 		'icon': '日式',
-		'defaultActivate': false,
+		'defaultActivate': False,
 		'childrens':[
 				{
 					'id': 'a3079e3b-10c6-4274-a3e1-b36406bc552e',
 					'name': '中式',
 					'icon': '日式',
-					'defaultActivate': false
+					'defaultActivate': False
 				},
 				{
 					'id': '34d6cf27-de11-4504-a980-32d9dcccdfc2',
 					'name': '美式',
 					'icon': '日式',
-					'defaultActivate': true
+					'defaultActivate': True
 				},
 				{
 					'id': 'a1365f37-bcb4-4183-adf3-26a83209b806',
 					'name': '日式',
 					'icon': '日式',
-					'defaultActivate': false
+					'defaultActivate': False
+				}
+			]
+		},
+		{
+		'id': 'f07bdd19-c156-4ad7-a296-e7f1c967ba5a',
+		'name': '鳥類',
+		'icon': '日式',
+		'defaultActivate': False,
+		'childrens':[
+				{
+					'id': '358e74bf-a2a7-40d5-8e4b-6e8a30f1459f',
+					'name': '中式',
+					'icon': '日式',
+					'defaultActivate': False
+				},
+				{
+					'id': '4dc5e881-48da-4fcc-8d76-e039d02e5f8f',
+					'name': '美式',
+					'icon': '日式',
+					'defaultActivate': True
+				},
+				{
+					'id': 'edfdccbd-d273-4124-aa27-4cd8409dd41d',
+					'name': '日式',
+					'icon': '日式',
+					'defaultActivate': False
 				}
 			]
 		}
-	])
+	]
+	return jsonify(filters)	
+
+@app.route("/"+app.config['API_VERSION']+"/places", methods=['POST'])
+def places():
+	return jsonify({
+		'test': 'test'
+	})				
+			
+@app.route("/"+app.config['API_VERSION']+"/place", methods=['GET'])
+def place():
+	return jsonify({
+		'test': 'test'
+	})	
