0ca02b3e3a205405a02ebb07d45f51618700b5bc
jderlin <jder@fungogo.xyz>
2018-05-03 11:15:29 +0800

    update

--- console/server/xai/SamplePage/route.py
+++ console/server/xai/SamplePage/route.py
@@ -55,7 +55,7 @@ def filters():
 					'id': '34d6cf27-de11-4504-a980-32d9dcccdfc2',
 					'name': '美式',
 					'icon': '日式',
-					'defaultActivate': True
+					'defaultActivate': False
 				},
 				{
 					'id': 'a1365f37-bcb4-4183-adf3-26a83209b806',
@@ -75,7 +75,7 @@ def filters():
 					'id': '358e74bf-a2a7-40d5-8e4b-6e8a30f1459f',
 					'name': '中式',
 					'icon': '日式',
-					'defaultActivate': False
+					'defaultActivate': True
 				},
 				{
 					'id': '4dc5e881-48da-4fcc-8d76-e039d02e5f8f',
@@ -90,18 +90,112 @@ def filters():
 					'defaultActivate': False
 				}
 			]
+		},
+		{
+		'id': '8c29988c-e5e3-4bb1-9f5e-b3f9b35410fd',
+		'name': '類別一',
+		'icon': '日式',
+		'defaultActivate': False,
+		'childrens':[
+				{
+					'id': 'eedb5172-2a14-48c0-983d-256422b0fe49',
+					'name': '中式',
+					'icon': '日式',
+					'defaultActivate': False
+				},
+				{
+					'id': '7d36e9ad-f2b1-475b-a4dc-f938b4c61ef6',
+					'name': '美式',
+					'icon': '日式',
+					'defaultActivate': False
+				},
+				{
+					'id': '22f443ea-1ece-469a-9633-9ed2bff3b0ff',
+					'name': '日式',
+					'icon': '日式',
+					'defaultActivate': False
+				}
+			]
+		},
+		{
+		'id': 'ccb81ab5-6f50-4d2b-ad62-e3c54018364a',
+		'name': '類別二',
+		'icon': '日式',
+		'defaultActivate': True
+		},
+		{
+		'id': 'a4d5b6c8-dc45-4763-8e54-8e0519444847',
+		'name': '類別三',
+		'icon': '日式',
+		'defaultActivate': False
+		},
+		{
+		'id': '404d398f-7520-4460-be22-629c30899d1c',
+		'name': '類別四',
+		'icon': '日式',
+		'defaultActivate': False
+		},
+		{
+		'id': '28bc0fe3-aa1c-4474-8258-6f29c430562b',
+		'name': '類別五',
+		'icon': '日式',
+		'defaultActivate': True
 		}
 	]
 	return jsonify(filters)	
 
 @app.route("/"+app.config['API_VERSION']+"/places", methods=['POST'])
 def places():
-	return jsonify({
-		'test': 'test'
-	})				
+	places = [		
+		{
+		'id': '36c4c67e-183c-4fb9-b95c-0f37a65953c5',
+		'title': '類別二',
+		'location': '日式',
+		'icon-url': True,
+		'path': True,
+		'images': [],
+		'fields':[]
+		},
+		{
+		'id': 'ce96991e-d65f-4b6b-9a38-2dfd6b52ef68',
+		'title': '類別二',
+		'location': '日式',
+		'icon-url': True,
+		'path': True,
+		'images': [],
+		'fields':[]
+		},
+		{
+		'id': '21ebb1dc-bd45-42c5-bfb2-c4e6fa5f2b48',
+		'title': '類別二',
+		'location': '日式',
+		'icon-url': True,
+		'path': True,
+		'images': [],
+		'fields':[]
+		},
+		{
+		'id': 'e310f53e-0023-4a33-8b5c-12a7e71ecb8c',
+		'title': '類別二',
+		'location': '日式',
+		'icon-url': True,
+		'path': True,
+		'images': [],
+		'fields':[]
+		}
+	]
+	return jsonify(places)					
 			
 @app.route("/"+app.config['API_VERSION']+"/place", methods=['GET'])
 def place():
-	return jsonify({
-		'test': 'test'
-	})	
+	place = {
+		'id': '21ebb1dc-bd45-42c5-bfb2-c4e6fa5f2b48',
+		'title': '鳥類',
+		'description': 'ddd',
+		'location': '日式',
+		'icon-url': True,
+		'path': True,
+		'images': [],
+		'fields':[]
+	}
+	return jsonify(place)	
